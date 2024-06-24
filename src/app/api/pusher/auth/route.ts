0da24886d4f72c2cf6f3d7093f5d6ffec756801c
yoshi-katsuhara <yoshihiro.katsuhara@oro.com>
2024-06-24 22:44:11 +0900

    認証ルートの作成

---
+++ src/app/api/pusher/auth/route.ts
@@ -0,0 +1,14 @@
+import { getPusherInstance } from "@/libs/pusher/server";
+const pusherServer = getPusherInstance();
+
+export async function POST(req: Request) {
+  console.log("authenticating pusher perms...")
+  const data = await req.text();
+  const [socketId, channelName] = data
+    .split("&")
+    .map((str) => str.split("=")[1]);
+
+  const authResponse = pusherServer.authorizeChannel(socketId, channelName);
+
+  return new Response(JSON.stringify(authResponse));
+}
